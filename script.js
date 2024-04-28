efc3b04940055c4d2138c039281985cb9233c195
Lorenzo <lorenzoribas47@gmail.com>
2024-04-28 17:14:39 -0300

    Delete function

--- script.js
+++ script.js
@@ -2,36 +2,48 @@ document.addEventListener("DOMContentLoaded", function() {
     let btn = document.querySelector("#add");
     let input = document.querySelector(".newtask input");
     let tasksContainer = document.querySelector(".tasks");
+    let emptyMessage = document.querySelector('.empty-message');
 
     btn.addEventListener("click", function() {
         if(input.value.trim().length === 0) {
             alert("Please enter a task before adding.");
         } else {
-            tasksContainer.innerHTML += `
-                <div class="task">
-                    <span class="taskname">
-                        ${input.value}
-                    </span>
-                
-                    <div class="btns">
-                        <button class="edit">
-                        <i class="fa-regular fa-pen-to-square"></i>
-                        </button>
-                        <button class="delete">
-                        <i class="fa-regular fa-trash-can"></i>
-                        </button>
-                    </div>
-                </div>`;
+            let newTaskElement = document.createElement('div');
+            newTaskElement.classList.add('task');
+            newTaskElement.innerHTML = `
+                <span class="taskname">
+                    ${input.value}
+                </span>
+                <div class="btns">
+                    <button class="edit">
+                        <i class="fas fa-edit"></i>
+                    </button>
+                    <button class="delete">
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                </div>
+            `;
             
+            tasksContainer.appendChild(newTaskElement);
             input.value = "";
-
-            let emptyMessage = document.querySelector('.empty-message');
-
-            let tasks0 = document.querySelector('.task').innerHTML;
-            if (tasks0.length > 0) {
-                emptyMessage.style.display = 'none';
-            }
-            
+            updateEmptyMessage();
+            addDeleteEventListener(newTaskElement.querySelector('.delete'));
         }
     });
+
+    function updateEmptyMessage() {
+        let tasksCount = tasksContainer.querySelectorAll('.task').length;
+        if (tasksCount > 0) {
+            emptyMessage.style.display = 'none';
+        } else {
+            emptyMessage.style.display = 'block';
+        }
+    }
+
+    function addDeleteEventListener(deleteButton) {
+        deleteButton.addEventListener('click', function() {
+            this.parentNode.parentNode.remove();
+            updateEmptyMessage();
+        });
+    }
 });
